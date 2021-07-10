7ab81ea96bdeaeff6ccb2e5457102647dcac8511
inorist <inorist6@gmail.com>
2021-07-10 18:07:27 +0900

    feature ステージセレクト機能の追加

---
+++ Assets/_SlimeCatch/SelectStage/Scripts/stage6scenetransition.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 1.UIシステムを使うときに必要なライブラリ
+using UnityEngine.UI;
+// 2.Scene関係の処理を行うときに必要なライブラリ
+using UnityEngine.SceneManagement;
+
+public class stage6scenetransition : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 3.OnRetry関数が実行されたら、sceneを読み込む
+    public void OnRetry()
+    {
+        // 「ButtonScene」を自分の読み込みたいscene名に変える
+        SceneManager.LoadScene("Stage6");
+    }
+}
