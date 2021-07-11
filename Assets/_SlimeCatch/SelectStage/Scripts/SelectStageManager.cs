dc7dec2033bad5e5f35744aadda9e6eb28f0a5fe
inorist6 <inorist6@gmail.com>
2021-07-12 00:18:02 +0900

    fix シーン遷移機能の修正

---
+++ Assets/_SlimeCatch/SelectStage/Scripts/SelectStageManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 1.UIシステムを使うときに必要なライブラリ
+using UnityEngine.UI;
+// 2.Scene関係の処理を行うときに必要なライブラリ
+using UnityEngine.SceneManagement;
+
+public class SelectStageManager : MonoBehaviour
+{
+    public void SelectStage1()
+    {
+        SceneManager.LoadSceneAsync("Stage1");
+    }
+
+    public void SelectStage2()
+    {
+        SceneManager.LoadSceneAsync("Stage2");
+    }
+
+    public void SelectStage3()
+    {
+        SceneManager.LoadSceneAsync("Stage3");
+    }
+
+    public void SelectStage4()
+    {
+        SceneManager.LoadSceneAsync("Stage4");
+    }
+
+    public void SelectStage5()
+    {
+        SceneManager.LoadSceneAsync("Stage5");
+    }
+
+    public void SelectStage6()
+    {
+        SceneManager.LoadSceneAsync("Stage6");
+    }
+
+}
