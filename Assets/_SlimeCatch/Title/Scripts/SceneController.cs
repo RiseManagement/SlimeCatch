bf296c6ae3f47cb376489df1732fc474d09c39f4
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-13 23:51:34 +0900

    feat: タイトル画面で左クリックをすると，ステージ選択シーンに遷移する

---
+++ Assets/_SlimeCatch/Title/Scripts/SceneController.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace _SlimeCatch.Title
+{
+    public class SceneController : MonoBehaviour
+    {
+        private void Start()
+        {
+            this.UpdateAsObservable().Where(_ => Input.GetMouseButtonDown(0))
+                .Subscribe(_ =>
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("SelectStage");
+                }).AddTo(this);
+        }
+    }
+}
