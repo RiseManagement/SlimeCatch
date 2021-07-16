f94dd5acacd6eb76bfb8c58bbe84fc77886ff9d1
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-16 21:56:32 +0900

    feat: ステージシーンでClearしたかどうかのデバッグ機能の追加

---
+++ Assets/_SlimeCatch/Stage/SaveTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using _SlimeCatch.Save;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace _SlimeCatch.Stage
+{
+    public class SaveTest : MonoBehaviour
+    {
+        private ISaveController _saveController;
+
+        private void Awake()
+        {
+            _saveController = new SaveLoadController();
+        }
+
+        private void Start()
+        {
+            this.UpdateAsObservable()
+                .Subscribe(_ =>
+                {
+                    if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        _saveController.Save(5,true);
+                    }else if (Input.GetKeyDown(KeyCode.R))
+                    {
+                        _saveController.Save(5,false);
+                    }
+                }).AddTo(this);
+        }
+    }
+}
