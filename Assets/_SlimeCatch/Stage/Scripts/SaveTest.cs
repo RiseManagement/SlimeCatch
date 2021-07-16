b35ab6839e05cd5910b3abb0692091f34634f8b5
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-16 22:36:14 +0900

    feat: Clear情報のデバッグシーンの作成と機能の追加

---
+++ Assets/_SlimeCatch/Stage/Scripts/SaveTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using _SlimeCatch.Save;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _SlimeCatch.Stage
+{
+    public class SaveTest : MonoBehaviour
+    {
+        private SaveLoadController _saveController;
+        [SerializeField] private List<GameObject> switchButtonList;
+        private readonly Subject<int> _onClickButtonSubject = new Subject<int>();
+
+        private void Awake()
+        {
+            _saveController = new SaveLoadController();
+            foreach (var (button,index) in switchButtonList.Select((button,index) => (button,index)))
+            {
+                button.GetComponent<Button>().OnClickAsObservable().Subscribe(_ =>
+                {
+                    _onClickButtonSubject.OnNext(index);
+                }).AddTo(this);
+            }
+
+            foreach (var (data,index) in _saveController.GetStageClearList().Select((data,index) => (data,index)))
+            {
+
+                switchButtonList[index].GetComponent<Image>().color = ChangeButtonColor(data);
+            }
+        }
+
+        private void Start()
+        {
+            _onClickButtonSubject.Subscribe(value =>
+            {
+                Debug.Log($"押されたボタン:{value}");
+                var clearData = _saveController.GetStageClearList()[value];
+                _saveController.Save(value,!clearData);
+                switchButtonList[value].GetComponent<Image>().color = ChangeButtonColor(!clearData);
+            }).AddTo(this);
+        }
+
+        private Color ChangeButtonColor(bool isClear)
+        {
+            return isClear ? Color.red : Color.blue;
+        }
+    }
+}
