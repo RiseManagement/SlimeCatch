3ae44a63c5d451dc1ea6ad015dbd81d7d842381e
hamanyan <hiroakipokemon@gmail.com>
2021-07-17 22:42:08 +0900

    feature Wave間隔を６秒で，敵が出てくるようにする
    ScriptableObject：各ステージ情報格納用　追加

---
+++ Assets/_SlimeCatch/Stage/Scripts/Wave/StageManagerObject.cs
@@ -0,0 +1,12 @@
+﻿using NaughtyAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _SlimeCatch.Wave
+{
+	[CreateAssetMenu(fileName = "StageInfo", menuName = "Create/StageInfo", order = 0)]
+	public class StageManagerObject : ScriptableObject
+	{
+		public List<WaveObject> waveObjects;
+	}
+}
