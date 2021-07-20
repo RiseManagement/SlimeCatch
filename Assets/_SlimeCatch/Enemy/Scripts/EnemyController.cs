0a5934ecf75b352b577545fb6718410be29cd5bc
DESKTOP-VO97ORH\Keisu <ekawano114@gmail.com>
2021-07-20 23:29:09 +0900

    fix 武器を交互に出し軌道も変更させるようにした

--- Assets/_SlimeCatch/Enemy/Scripts/EnemyController.cs
+++ Assets/_SlimeCatch/Enemy/Scripts/EnemyController.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using _SlimeCatch.Enemy;
 using _SlimeCatch.Weapon;
 using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Linq;
 using DG.Tweening;
 using NaughtyAttributes;
 using UnityEngine;
-
 public class EnemyController : MonoBehaviour
 {
     [SerializeField,Label("移動距離(ベクトル)"),Range(1,5)] private float moveDistance;
@@ -14,6 +14,10 @@ public class EnemyController : MonoBehaviour
     [SerializeField,Label("移動時間"),Range(1,5)] private float moveTime;
 
     [SerializeField] private List<GameObject> weaponList;
+
+    [SerializeField] private EnemyObject enemyObject;
+
+    [SerializeField] private WeaponNameDecision _weaponNameDecision;
     
     // Start is called before the first frame update
     private async void Start()
@@ -31,10 +35,22 @@ public class EnemyController : MonoBehaviour
 
     private async UniTask ThrowWeapon()
     {
-        await weaponList.ToUniTaskAsyncEnumerable().ForEachAwaitAsync(async x =>
+        var isBaseWeapon = false;
+        var weaponOrbit=WeaponOrbitEnum.None;
+        await weaponList.ToUniTaskAsyncEnumerable().ForEachAwaitAsync(async x  =>
         {
             var weaponObject = Instantiate(x, transform);
-            weaponObject.GetComponent<IWeaponMove>().WeaponMove(new Vector3(5,0,0),WeaponOrbitEnum.Curve);
+            if (isBaseWeapon)
+            {
+                weaponOrbit = _weaponNameDecision.WeaponOrbitSearch(enemyObject.BaseWeapon);
+                isBaseWeapon = false;
+            }
+            else
+            {
+                weaponOrbit = _weaponNameDecision.WeaponOrbitSearch(enemyObject.SpecialWeapon);
+                isBaseWeapon = true;
+            }
+            weaponObject.GetComponent<IWeaponMove>().WeaponMove(new Vector3(5,0,0),weaponOrbit);
             await UniTask.Delay(TimeSpan.FromSeconds(2f));
             Destroy(weaponObject);
         });
