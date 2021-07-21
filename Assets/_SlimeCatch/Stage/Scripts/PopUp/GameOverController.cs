b4f1b5df019a9d41ac9c15548c8d89844b81c427
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-21 20:58:54 +0900

    feat: ゲームオーバーしたときのポップアップの作成

---
+++ Assets/_SlimeCatch/Stage/Scripts/PopUp/GameOverController.cs
@@ -0,0 +1,28 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _SlimeCatch.Stage.PopUp
+{
+    public class GameOverController : MonoBehaviour
+    {
+        [SerializeField] private Button retryButton;
+        [SerializeField] private Button backStageSelectButton;
+
+        private void Start()
+        {
+            retryButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                Debug.Log("retry");
+                gameObject.SetActive(false);
+            }).AddTo(this);
+
+            backStageSelectButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                Debug.Log("back stage");
+                SceneManager.LoadSceneAsync("_SlimeCatch/SelectStage/SelectStage");
+            }).AddTo(this);
+        }
+    }
+}
