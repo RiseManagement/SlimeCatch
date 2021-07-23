8f1d2b899b2394a0887531cf2255ff8e48b4d426
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-23 16:19:14 +0900

    feat: ステージ４で開始前にフェードインになるようにした

---
+++ Assets/_SlimeCatch/Stage/Scripts/StartFadeIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _SlimeCatch.Stage
+{
+    public class StartFadeIn : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer player;
+        [SerializeField] private SpriteRenderer backGround;
+        [SerializeField] private List<SpriteRenderer> childList;
+
+        private const float FadeInAnimationTime = 1f;
+        private const Ease FadeInAnimationEase = Ease.InSine;
+
+        private void Start()
+        {
+            foreach (var child in childList)
+            {
+                child.DOFade(1f, FadeInAnimationTime).SetEase(FadeInAnimationEase);
+            }
+            player.DOFade(1f, FadeInAnimationTime).SetEase(FadeInAnimationEase);
+            backGround.DOFade(1f, FadeInAnimationTime).SetEase(FadeInAnimationEase);
+        }
+    }
+}
