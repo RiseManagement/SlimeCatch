7b2a8cfa8628382844d69c9e9a668d84f8cc1158
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-23 20:04:46 +0900

    feat: ステージアイコン間の接続アニメーションの実装

---
+++ Assets/_SlimeCatch/SelectStage/Scripts/ConnectStageAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _SlimeCatch.SelectStage
+{
+    public class ConnectStageAnimation : MonoBehaviour
+    {
+        [SerializeField] private List<Image> nextStageImage;
+        [SerializeField] private float connectFadeAnimation = 2f;
+
+
+        private void Start()
+        {
+            //todo デバッグ用
+            this.UpdateAsObservable().Where(_ => Input.GetKeyDown(KeyCode.Alpha6))
+                .Subscribe(_ =>
+                {
+                    SetAnimation(0);
+                }).AddTo(this);
+        }
+
+        public void SetAnimation(int nextStageNo)
+        {
+            nextStageImage[nextStageNo].DOFillAmount(1f, connectFadeAnimation);
+        }
+    }
+}
