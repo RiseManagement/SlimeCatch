2f2dc1620da3718e973c40c97c4a28e1f31521ea
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-27 00:35:12 +0900

    style: スクリプト名の変更とコード整理

---
+++ Assets/_SlimeCatch/Player/Scripts/ChildrenSlimeWeaponCollider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ChildrenSlimeWeaponCollider : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Weapon")) return;
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
