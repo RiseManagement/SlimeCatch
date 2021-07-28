305e1ca9bd44462ed54820238d7dd7da74ac8a03
LuGeNe0N <lie10210@gmail.com>
2021-07-28 17:46:04 +0900

    feature 親スライムを掴んでいないときは当たり判定を発生させない機能
    ＊ 親スライムのレイヤーを変更するスクリプトを追加
    ＊ ”CollisionOff”レイヤーを追加
    ＊ プロジェクト設定でレイヤー"Default"と"CollisionOff"の衝突判定を無効化

---
+++ Assets/_SlimeCatch/Stage/Player/Scripts/CollisionChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChange : MonoBehaviour
+{
+    [SerializeField] _SlimeCatch.Player.SlimeExtend SlimeExtend;
+    [SerializeField] private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //掴んでいる時に減らす
+        if (SlimeExtend._initMousePosition != SlimeExtend._CurrentMousePosition)
+        {
+            player.layer = LayerMask.NameToLayer("Default");
+        }
+        else
+        {
+            player.layer = LayerMask.NameToLayer("CollisionOff");
+        }
+    }
+}
