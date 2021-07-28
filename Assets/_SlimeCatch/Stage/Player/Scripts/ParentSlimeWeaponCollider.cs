872da564585d156a8d70122c68ad5e5fb5f813aa
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-28 17:59:44 +0900

    style: namespaceの追加

--- Assets/_SlimeCatch/Stage/Player/Scripts/ParentSlimeWeaponCollider.cs
+++ Assets/_SlimeCatch/Stage/Player/Scripts/ParentSlimeWeaponCollider.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ParentSlimeWeaponCollider : MonoBehaviour
+namespace _SlimeCatch.Player
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    public class ParentSlimeWeaponCollider : MonoBehaviour
     {
-        if (!other.gameObject.CompareTag("Weapon")) return;
-        Destroy(other.gameObject);
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!other.gameObject.CompareTag("Weapon")) return;
+            Destroy(other.gameObject);
+        }
     }
+    
 }
