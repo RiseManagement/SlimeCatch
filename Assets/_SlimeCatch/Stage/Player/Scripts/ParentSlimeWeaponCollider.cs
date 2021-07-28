0755c1cb802c0e1e0864a631d49c7a77c3221926
DESKTOP-VO97ORH\Keisu <ekawano114@gmail.com>
2021-07-28 17:14:22 +0900

    fix 親スライムに武器が当たると武器が消えるスクリプトの追加

---
+++ Assets/_SlimeCatch/Stage/Player/Scripts/ParentSlimeWeaponCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentSlimeWeaponCollider : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Weapon")) return;
+        Destroy(other.gameObject);
+    }
+}
