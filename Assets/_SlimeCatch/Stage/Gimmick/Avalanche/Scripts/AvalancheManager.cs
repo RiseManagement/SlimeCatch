a5c27bc1e60f3071c6d23df6f18a0d5cb7e98263
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-31 22:46:54 +0900

    feat: 画面の揺れの実装

---
+++ Assets/_SlimeCatch/Stage/Gimmick/Avalanche/Scripts/AvalancheManager.cs
@@ -0,0 +1,16 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace _SlimeCatch.Stage.Gimmick.Avalanche.Scripts
+{
+    public class AvalancheManager : MonoBehaviour
+    {
+        [SerializeField] private CameraShake cameraShake;
+
+        [Button("画面を揺らす")]
+        public void ShakeTest()
+        {
+            cameraShake.Shake();
+        }
+    }
+}
