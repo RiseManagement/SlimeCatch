0930367a170e168b15d437ce869dd324f8e28bbd
ayutaz <ka1357amnbpdr@gmail.com>
2021-07-31 21:11:27 +0900

    fix: リファクタリングとすぐに行動バーが減らない問題の修正

---
+++ Assets/_SlimeCatch/Stage/Player/Scripts/ParentActiveBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _SlimeCatch.Player
+{
+    public class ParentActiveBar : MonoBehaviour
+    {
+        [SerializeField] private Slider activeSlider;
+        public void SetActiveValue(float value)
+        {
+            activeSlider.value = value;
+        }
+    }
+}
--- Assets/_SlimeCatch/Stage/Player/Scripts/SlimeExtend.cs
+++ Assets/_SlimeCatch/Stage/Player/Scripts/SlimeExtend.cs
@@ -6,12 +6,13 @@ namespace _SlimeCatch.Player
     public class SlimeExtend : MonoBehaviour
     {
         private Vector3 _mMouseDownPosition = Vector3.zero;
-        public Vector3 _initMousePosition = Vector3.zero;
-        public Vector3 _CurrentMousePosition = Vector3.zero;
-        public bool _GaugeEmpty = false;
+        public bool gaugeEmpty;
+        private bool _isExtend;
+        private float _activeValue = 5f;
         private BoxCollider2D _mCollider;
         private const float MScaleX = 2.677196f;
         private CollisionChange _collisionChange;
+        [SerializeField] private ParentActiveBar parentActiveBar;
 
         private void Awake()
         {
@@ -19,19 +20,35 @@ namespace _SlimeCatch.Player
             _mCollider = GetComponent<BoxCollider2D>();
         }
 
+        private void Update()
+        {
+            if (_isExtend)
+            {
+                _activeValue -= Time.deltaTime;
+            }
+            else
+            {
+                if (_activeValue <= 5f)
+                {
+                    _activeValue += Time.deltaTime;
+                }
+                
+            }
+            parentActiveBar.SetActiveValue(_activeValue);
+        }
+
         private void OnMouseDown()
         {
             // マウスクリックした際の初期位置を保存。
             _mMouseDownPosition = transform.position;
-            _initMousePosition = transform.position;
         }
 
         private void OnMouseDrag()
         {
+            _isExtend = true;
             _collisionChange.ChangeLayer(true);
-            if (_GaugeEmpty)
+            if (gaugeEmpty)
             {
-                _CurrentMousePosition = _initMousePosition;
                 transform.position = _mMouseDownPosition;
                 transform.rotation = Quaternion.identity;
                 transform.localScale = Vector3.one;
@@ -41,7 +58,6 @@ namespace _SlimeCatch.Player
             var inputPosition   = new Vector3( Input.mousePosition.x, Input.mousePosition.y, 9.5f );
             //スクリーン座標をワールド座標に変換する
             var mousePos = Camera.main.ScreenToWorldPoint( inputPosition );
-            _CurrentMousePosition = inputPosition;
             var dist = Vector3.Distance( mousePos, _mMouseDownPosition );
 
             var distX = Mathf.Clamp(dist, 0.05f, 4.0f);
@@ -54,13 +70,13 @@ namespace _SlimeCatch.Player
 
         private void OnMouseUp()
         {
+            _isExtend = false;
             _collisionChange.ChangeLayer(false);
             // 位置、回転、スケールを元に戻す。
             transform.position      = _mMouseDownPosition;
             transform.rotation      = Quaternion.identity;
             transform.localScale    = Vector3.one;
-            _CurrentMousePosition = _initMousePosition;
-            _GaugeEmpty = false;
+            gaugeEmpty = false;
         }
     }
 }
