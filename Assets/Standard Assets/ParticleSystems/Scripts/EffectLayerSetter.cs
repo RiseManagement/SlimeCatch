2c56ce9c1c4b85e841f4fd60a7fb70ffbee88260
DESKTOP-VO97ORH\Keisu <ekawano114@gmail.com>
2021-08-01 15:04:16 +0900

    fix stage2に砂嵐エフェクトの追加

---
+++ Assets/Standard Assets/ParticleSystems/Scripts/EffectLayerSetter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectLayerSetter : MonoBehaviour
+{
+    //エフェクト用のレイヤー名
+    private string EFFECT_SORTING_LAYER_NAME = "Effect";
+
+    private void Awake()
+    {
+        SetSortingLayer(transform);
+    }
+
+    private void SetSortingLayer(Transform parent)
+    {
+        //レンダラーがある場合のみレイヤーを設定
+        //if (parent.gameObject.renderer) {
+        //parent.gameObject.renderer.sortingLayerName = EFFECT_SORTING_LAYER_NAME;
+        // }
+
+        //子がいる場合には、それにも同じ処理を行う
+        foreach (Transform child in parent.transform)
+        {
+            SetSortingLayer(child);
+        }
+    }
+}
