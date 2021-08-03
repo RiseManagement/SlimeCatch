b5d7672e4feceb6c9ff9ae7d6f76f307e14d289c
hamanyan <hiroakipokemon@gmail.com>
2021-08-03 21:43:18 +0900

    feat：タイトル画面からステージセレクト画面のBGMの引継ぎ処理の試し

---
+++ Assets/_SlimeCatch/Title/Scripts/TitleBgmController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _SlimeCatch.Title
+{
+	public class TitleBgmController : MonoBehaviour
+	{
+		public bool DontDestroyEnabled = true;
+		// Start is called before the first frame update
+		void Start()
+		{
+			if (DontDestroyEnabled)
+			{
+				// Sceneを遷移してもオブジェクトが消えないようにする
+				DontDestroyOnLoad(this);
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
