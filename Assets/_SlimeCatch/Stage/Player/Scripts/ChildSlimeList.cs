114984275fa8bb951d9f50c30b2ccd4519f97d27
ayutaz <ka1357amnbpdr@gmail.com>
2021-08-04 10:29:33 +0900

    feat: ChildSlimeList.csからすべての子供スライムの浮かぶアニメーションを実行可能に変更とテストの追加

--- Assets/_SlimeCatch/Stage/Player/Scripts/ChildSlimeList.cs
+++ Assets/_SlimeCatch/Stage/Player/Scripts/ChildSlimeList.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using _SlimeCatch.Stage.Gimmick.Wetland.Scripts;
+using NaughtyAttributes;
 using UniRx;
 using UnityEngine;
 
@@ -10,6 +12,12 @@ public class ChildSlimeList : MonoBehaviour
     private readonly Subject<int> _damageSlimeChild = new Subject<int>();
     private const int MAXOffActiveCount = 2;
     private readonly List<GameObject> _aliveChildSlimeList = new List<GameObject>();
+
+    [Button("ChildrenFloat")]
+    private void ChildrenFloatTest()
+    {
+        ChildrenFloat();
+    }
 
     private void Awake()
     {
@@ -57,5 +65,13 @@ public class ChildSlimeList : MonoBehaviour
     {
         var r = Random.Range(0, _aliveChildSlimeList.Count-1);
         return _aliveChildSlimeList[r].transform.position;
+    }
+
+    public void ChildrenFloat()
+    {
+        foreach (var child in _slimeChild)
+        {
+            child.GetComponent<ChildAnimationHandler>()?.ChildFloat();
+        }
     }
 }
