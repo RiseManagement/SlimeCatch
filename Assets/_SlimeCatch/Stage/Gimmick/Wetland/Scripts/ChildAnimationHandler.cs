9a1010ad530e08569d76bd19d9fd05c164953c76
ayutaz <ka1357amnbpdr@gmail.com>
2021-08-04 10:01:52 +0900

    feat: 沼地に沈むアニメーションと浮かぶアニメーションの追加

---
+++ Assets/_SlimeCatch/Stage/Gimmick/Wetland/Scripts/ChildAnimationHandler.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace _SlimeCatch.Stage.Gimmick.Wetland.Scripts
+{
+    public class ChildAnimationHandler : MonoBehaviour
+    {
+        private const float SinkPositionY = -5.5f;
+        private const float AnimationTime = 15f;
+        private float _firstPositionY;
+        private Tweener _tweeter;
+
+        private void Awake()
+        {
+            _firstPositionY = transform.position.y;
+        }
+
+        [Button("SinkTest")]
+        public void SinkTest()
+        {
+            ChildSink();
+        }
+
+        [Button("FloatTest")]
+        private void FloatTest()
+        {
+            ChildFloat();
+        }
+
+        public void ChildFloat()
+        {
+            _tweeter = transform.DOMoveY(_firstPositionY, AnimationTime);
+        }
+
+        public void ChildSink()
+        {
+           _tweeter = transform.DOMoveY(SinkPositionY, AnimationTime);
+        }
+
+        private void OnDestroy()
+        {
+            _tweeter.Kill();
+        }
+    }
+}
