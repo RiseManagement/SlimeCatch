9b78fb20f228ee9eac574ca34f751f9977932ca4
hamanyan <hiroakipokemon@gmail.com>
2021-08-05 23:53:36 +0900

    fix：UniTaskの処理に切り替えた

--- Assets/_SlimeCatch/Title/Scripts/TitleBgmController.cs
+++ Assets/_SlimeCatch/Title/Scripts/TitleBgmController.cs
@@ -1,26 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
-namespace _SlimeCatch.Title
+namespace _SlimeCatch.Title.Bgm
 {
 	public class TitleBgmController : MonoBehaviour
 	{
-		public bool DontDestroyEnabled = true;
+		private Scene nowscene;
+
 		// Start is called before the first frame update
-		void Start()
+		private  async void Start()
 		{
-			if (DontDestroyEnabled)
-			{
-				// Sceneを遷移してもオブジェクトが消えないようにする
-				DontDestroyOnLoad(this);
-			}
+			DontDestroyOnLoad(this.gameObject);
+			await UniTask.WaitUntil(() => isdeleteBgmScene());
+			TitleBgmObjectDelete();
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
 
+		}
+
+		private bool isdeleteBgmScene()
+		{
+			 nowscene = SceneManager.GetActiveScene();
+
+			if (nowscene.name == "Title") return false;
+			if (nowscene.name == "SelectStage") return false;
+
+			return true;
+		}
+
+		void TitleBgmObjectDelete()
+		{
+			Destroy(this.gameObject);
 		}
 	}
 }
