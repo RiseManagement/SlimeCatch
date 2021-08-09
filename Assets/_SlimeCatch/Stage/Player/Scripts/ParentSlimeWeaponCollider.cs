753f665e6d07f86bc04a60ad9f89caccfef76d3a
ayutaz <ka1357amnbpdr@gmail.com>
2021-08-09 21:52:27 +0900

    fix: タグの整理と火炎瓶の当たり判定の修正

--- Assets/_SlimeCatch/Stage/Player/Scripts/ParentSlimeWeaponCollider.cs
+++ Assets/_SlimeCatch/Stage/Player/Scripts/ParentSlimeWeaponCollider.cs
@@ -6,11 +6,11 @@ namespace _SlimeCatch.Player
     {
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.CompareTag("Weapon"))
+            if (other.gameObject.CompareTag("Weapon/MolotovCocktail") || other.gameObject.CompareTag("Weapon/OtherWeapon"))
             {
                 GetComponent<SlimesReceiveSE>().ReceiveSe();
             }
-            if (other.gameObject.CompareTag("Weapon") || other.gameObject.CompareTag("Gimmick"))
+            if (other.gameObject.CompareTag("Weapon/MolotovCocktail") || other.gameObject.CompareTag("Weapon/OtherWeapon")|| other.gameObject.CompareTag("Gimmick"))
             {
                 Destroy(other.gameObject);
             }
